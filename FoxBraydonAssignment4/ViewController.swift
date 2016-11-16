c40fc27be30c5cfe6c5a3cbd26b46c641c141c6d
Braydon Fox <braydonfox@braydons-mac.local>
2016-11-15 20:28:36 -0500

    Added functionality to populate the studentTableView from JSON results.

--- FoxBraydonAssignment4/ViewController.swift
+++ FoxBraydonAssignment4/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    @IBOutlet weak var studentsTableView: UITableView!
+    
+    var studentTableData:NSArray = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        studentsTableView.delegate = self
+        studentsTableView.dataSource = self
         self.title = "Students"
-        // Do any additional setup after loading the view, typically from a nib.
+        let urlAsString = "http://csmadison.dhcp.bsu.edu/~vjtanksale/cs320/selectstudents.php"
+        let url = URL(string: urlAsString)
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: url!, completionHandler: { (data, response, error) -> Void in
+            if(error != nil) {
+                print(error!.localizedDescription)
+            }
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSArray {
+                    DispatchQueue.main.async(execute: {
+                        self.studentTableData = jsonResult
+                        self.studentsTableView.reloadData()
+                    })
+                }
+            }
+            catch {}
+        })
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -26,11 +47,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return studentTableData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier:"cell")
+        let rowData:NSDictionary = (self.studentTableData[indexPath.row] as? NSDictionary)!
+        cell.textLabel?.text = (rowData["FirstName"] as? String)! + " " + (rowData["LastName"] as? String)!
         return cell
     }
 
